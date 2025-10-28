0a24dc328e77b46309a2571a452005bd6ae57ac7
renso93 <renso93.info@gmail.com>
2025-10-28 02:48:03 +0000

    Ajout des bases de developpement et de production

---
+++ src/blog/settings/developpement.py
@@ -0,0 +1,15 @@
+from ..settings import *
+
+DEBUG = True
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+
+# Database pour le développement
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+
+# Email backend pour le développement
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
---
+++ src/blog/settings/production.py
@@ -0,0 +1,31 @@
+from ..settings import *
+
+DEBUG = False
+ALLOWED_HOSTS = config('AllOWED_HOSTS', cast=lambda v: [s.strip() for s in v.split(',')])
+
+# Sécurité
+SECURE_SSL_REDIRECT = True
+SECURE_HSTS_SECONDS = 31536000
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
+# Cache avec Redis
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        'LOCATION': config('REDIS_URL', default='redis://127.0.0.1:6379/1'),
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+        }
+    }
+}
+
+# Email backend pour la production
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = config('EMAIL_HOST', default='smtp.gmail.com')
+EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
