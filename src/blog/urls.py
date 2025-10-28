e85265c0530c8f3bd21d585f924915d97f0890e2
renso93 <renso93.info@gmail.com>
2025-10-28 13:45:10 +0000

    ajout de base.html

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("apps.blogpost.urls")),
+    path("", include("blogpost.urls")),
     #path("api/", include("apps.blogpost.api_urls")),
     path('accounts/', include("django.contrib.auth.urls")),
 ]
